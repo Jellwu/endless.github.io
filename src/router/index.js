1205e6659b3c6a8dc7294ef2d77bdd2578d88cdb
Jell <jell9916@gmail.com>
2020-05-14 15:31:00 +0800

    adjust

--- src/router/index.js
+++ src/router/index.js
@@ -14,76 +14,76 @@ export default new VueRouter({
     {
       path: '/',
       name: 'Home',
-      component: () => import('../views/fontend/Home.vue')
+      component: () => import('../views/Fontend/Home.vue')
     },
     {
       path: '/profile',
       name: 'Profile',
-      component: () => import('../views/fontend/Profile.vue')
+      component: () => import('../views/Fontend/Profile.vue')
     },
     {
       path: '/productList',
       name: 'Products',
-      component: () => import('../views/fontend/Products.vue')
+      component: () => import('../views/Fontend/Products.vue')
     },
     {
       path: '/productList/:productID',
       name: 'Productsdetail',
-      component: () => import('../views/fontend/page/ProductDetail.vue')
+      component: () => import('../views/Fontend/Page/ProductDetail.vue')
     },
     {
       path: '/cart',
       name: 'Cart',
-      component: () => import('../views/fontend/Cart.vue')
+      component: () => import('../views/Fontend/Cart.vue')
     },
     {
       path: '/cart_info',
       name: 'CartInfo',
-      component: () => import('../views/fontend/page/Cart_info.vue')
+      component: () => import('../views/Fontend/Page/Cart_info.vue')
     },
     {
       path: '/cart_checkout/:orderID',
       name: 'cart_checkout',
-      component: () => import('../views/fontend/page/Cart_checkout.vue')
+      component: () => import('../views/Fontend/Page/Cart_checkout.vue')
     },
     {
       path: '/cart_orderpay/:orderID',
       name: 'cart_orderpay',
-      component: () => import('../views/fontend/page/Cart_orderpay.vue')
+      component: () => import('../views/Fontend/Page/Cart_orderpay.vue')
     },
     {
       path: '/couponpage',
       name: 'couponpage',
-      component: () => import('../views/fontend/Couponpage.vue')
+      component: () => import('../views/Fontend/Couponpage.vue')
     },
     // 後台管理
     {
       path: '/login',
       name: 'Login',
-      component: () => import('../views/backend/Login.vue')
+      component: () => import('../views/Backend/Login.vue')
     },
     {
       path: '/admin',
       name: 'Admin',
-      component: () => import('../views/backend/Admin.vue'),
+      component: () => import('../views/Backend/Admin.vue'),
       meta: { requiresAuth: true },
       children: [
         {
           path: 'productscheck',
           name: 'Productscheck',
-          component: () => import('../views/backend/Dashboard/Productscheck.vue'),
+          component: () => import('../views/Backend/Dashboard/Productscheck.vue'),
           meta: { requiresAuth: true }
         },
         {
           path: 'orders',
           name: 'orders',
-          component: () => import('../views/backend/Dashboard/Orders.vue'),
+          component: () => import('../views/Backend/Dashboard/Orders.vue'),
           meta: { requiresAuth: true }
         },
         {
           path: 'coupon',
           name: 'coupon',
-          component: () => import('../views/backend/Dashboard/Coupon.vue'),
+          component: () => import('../views/Backend/Dashboard/Coupon.vue'),
           meta: { requiresAuth: true }
         }
       ]
